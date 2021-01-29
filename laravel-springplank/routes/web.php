e6df7812f1fd7d9c880d3956db5d974e8678669f
David <60575606+97060324@users.noreply.github.com>
2021-01-29 10:47:51 +0100

    Added contactpage

--- laravel-springplank/routes/web.php
+++ laravel-springplank/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/contact', [ContactController::class, 'index'])->name('contact');
 
 Route::get('home', function () {
     return view('home');
