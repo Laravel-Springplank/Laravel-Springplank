67a978e28013ec65c57657138af8833cbe47b657
Romeo Lie-A-Tjam <romeogregoryx@gmail.com>
2021-01-29 10:33:07 +0100

    Added roles & email verification

--- laravel-springplank/routes/web.php
+++ laravel-springplank/routes/web.php
@@ -13,8 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::view('home', 'home');
+Route::get('home', function () {
+    return view('home');
+})->middleware('verified');
