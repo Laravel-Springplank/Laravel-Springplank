d6c73227714b4fb412a514efa19756a4225cbc84
David <60575606+97060324@users.noreply.github.com>
2021-02-15 15:52:18 +0100

    Newsletter dingen

--- laravel-springplank/app/Http/Controllers/NewsletterController.php
+++ laravel-springplank/app/Http/Controllers/NewsletterController.php
@@ -15,20 +15,6 @@ class NewsletterController extends Controller
         return view('newsletter');
     }
 
-<<<<<<< Updated upstream
-    public function store(Newsletter $newsletter)
-    {
-        // dd(request()->email);
-        if ( ! Newsletter::isSubscribed(request()->email) )
-        {
-            Newsletter::subscribePending(request()->email);
-            return redirect('newsletter')->with('success', 'Thanks For Subscribe');
-=======
-//    /**
-//     *  @param \App\Models\Newsletter
-//     *  @return \Illuminate\Http\Response
-//     */
-
     public function store(Request $request)
     {
         $data = $request->validate([
@@ -39,7 +25,6 @@ class NewsletterController extends Controller
         {
             Newsletter::subscribePending($request->email);
             return redirect()->back()->with('success', 'Thanks For Subscribe');
->>>>>>> Stashed changes
         }
         return redirect()->back()->with('success', 'Thanks For Subscribe');
 
